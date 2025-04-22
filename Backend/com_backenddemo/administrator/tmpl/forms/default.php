bd92a182406d5db035a8dead79ae6c40f8b33d1b
aukaheng <aukhhome@hotmail.com>
2025-04-22 19:19:40 +0800

    Update default.php

--- Backend/com_backenddemo/administrator/tmpl/forms/default.php
+++ Backend/com_backenddemo/administrator/tmpl/forms/default.php
@@ -1,1 +1,17 @@
+<?php
 
+use Joomla\CMS\Router\Route;
+
+\defined('_JEXEC') or die;
+
+?>
+
+<form action="<?php echo Route::_('index.php?option=com_backenddemo&view=forms'); ?>" method="post" name="adminForm" id="adminForm">
+</form>
+
+<script>
+    Joomla.submitbutton = (task) => {
+
+        Joomla.submitform(task);
+    };
+</script>
