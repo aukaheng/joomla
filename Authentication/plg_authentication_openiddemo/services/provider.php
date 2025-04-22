65a79fc5650810d0d129388c9e3f945b14530c3b
aukaheng <aukhhome@hotmail.com>
2025-04-22 12:31:33 +0800

    Create provider.php

---
+++ Authentication/plg_authentication_openiddemo/services/provider.php
@@ -0,0 +1,34 @@
+<?php
+
+use AKH\Plugin\Authentication\OpenIDDemo\Extension\OpenIDDemo;
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Database\DatabaseInterface;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+
+\defined('_JEXEC') or die;
+
+return new class () implements ServiceProviderInterface {
+    public function register(Container $container)
+    {
+        $container->set(
+            PluginInterface::class,
+            function (Container $container) {
+                $dispatcher = $container->get(DispatcherInterface::class);
+
+                $plugin = new OpenIDDemo(
+                    $dispatcher,
+                    (array) PluginHelper::getPlugin('authentication', 'openiddemo')
+                );
+        
+                $plugin->setApplication(Factory::getApplication());
+                $plugin->setDatabase($container->get(DatabaseInterface::class));
+
+                return $plugin;
+            }
+        );
+    }
+};
