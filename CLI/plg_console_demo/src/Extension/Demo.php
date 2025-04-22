f2745e1d666be46c484aa58d680c28f3aeed05d9
aukaheng <aukhhome@hotmail.com>
2025-04-22 12:17:32 +0800

    Create Demo.php

---
+++ CLI/plg_console_demo/src/Extension/Demo.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AKH\Plugin\Console\Demo\Extension;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Event\SubscriberInterface;
+use Joomla\Application\ApplicationEvents;
+use Joomla\Database\DatabaseAwareTrait;
+use AKH\Plugin\Console\Demo\CliCommand\DoSomething;
+
+class Demo extends CMSPlugin implements SubscriberInterface
+{
+    use DatabaseAwareTrait;
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ApplicationEvents::BEFORE_EXECUTE => 'registerCommands'
+        ];
+    }
+
+    public function registerCommands(): void
+    {
+        $this->getApplication()->addCommand(new DoSomething($this->getDatabase()));
+    }
+}
