d6df01ef5b283096e14dd3ba9c94adb2f9497e52
aukaheng <aukhhome@hotmail.com>
2025-05-06 15:08:12 +0800

    Create service provider

---
+++ Plugin/plg_user_contaunica/services/provider.php
@@ -0,0 +1,33 @@
+<?php
+
+use AKH\Plugin\User\ContaUnica\Extension\ContaUnica;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Database\DatabaseInterface;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+
+\defined('_JEXEC') or die;
+
+return new class () implements ServiceProviderInterface {
+    public function register(Container $container): void
+    {
+        $container->set(
+            PluginInterface::class,
+            function (Container $container) {
+                $dispatcher = $container->get(DispatcherInterface::class);
+                $plugin = new ContaUnica(
+                    $dispatcher,
+                    (array) PluginHelper::getPlugin('user', 'contaunica')
+                );
+                $plugin->setApplication(Factory::getApplication());
+                $plugin->setDatabase($container->get(DatabaseInterface::class));
+
+                return $plugin;
+            }
+        );
+    }
+};
