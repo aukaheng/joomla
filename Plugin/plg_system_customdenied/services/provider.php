da3357dddd87d0eaf22919088c1a630ba5d55b51
aukaheng <aukhhome@hotmail.com>
2025-06-08 00:43:29 +0800

    Update provider.php

--- Plugin/plg_system_customdenied/services/provider.php
+++ Plugin/plg_system_customdenied/services/provider.php
@@ -1,1 +1,33 @@
+<?php
 
+use AKH\Plugin\System\CustomDenied\Extension\CustomDenied;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Database\DatabaseInterface;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+
+\defined('_JEXEC') or die;
+
+return new class () implements ServiceProviderInterface {
+    public function register(Container $container): void
+    {
+        $container->set(
+            PluginInterface::class,
+            function (Container $container) {
+                $dispatcher = $container->get(DispatcherInterface::class);
+                $plugin = new CustomDenied(
+                    $dispatcher,
+                    (array) PluginHelper::getPlugin('system', 'customdenied')
+                );
+                $plugin->setApplication(Factory::getApplication());
+                $plugin->setDatabase($container->get(DatabaseInterface::class));
+
+                return $plugin;
+            }
+        );
+    }
+};
