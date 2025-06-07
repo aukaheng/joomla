c41693e63d9140020862445af057f6054a95fe0e
aukaheng <aukhhome@hotmail.com>
2025-06-08 00:49:22 +0800

    Update CustomDenied.php

--- Plugin/plg_system_customdenied/src/Extension/CustomDenied.php
+++ Plugin/plg_system_customdenied/src/Extension/CustomDenied.php
@@ -1,1 +1,40 @@
+<?php
 
+namespace AKH\Plugin\System\CustomDenied\Extension;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Application\SiteApplication;
+use Joomla\CMS\Language\Text;
+
+final class CustomDenied extends CMSPlugin
+{
+    public function onAfterRoute()
+    {
+        $app = Factory::getApplication();
+
+        if (!$app instanceof SiteApplication || $app->isClient('administrator')) {
+            return;
+        }
+
+        $menu = $app->getMenu();
+        $active = $menu->getActive();
+
+        if (!$active) {
+            return;
+        }
+
+        $user = Factory::getUser();
+
+        if (!in_array($active->access, $user->getAuthorisedViewLevels())) {
+            $redirectUrl = Route::_('index.php?option=com_somewhere', false);
+            $message = Text::_('PLG_SYSTEM_CUSTOMDENIED_ACCESS_DENIED');
+
+            $app->redirect($redirectUrl, $message, 'error');
+            $app->close();
+        }
+    }
+}
